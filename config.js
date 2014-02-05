754f71c97775fda82103a257a263db2069b562e8
Dejan Ranisavljevic <dejan.ranisavljevic@saturized.com>
2014-02-05 15:00:12 +0100

    Add config parsing and checking.

---
+++ config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var parseJSON = function(data) {
+  try {
+    return JSON.parse(data);
+  } catch (e) {
+    return undefined;
+  }
+};
+
+exports.exists = function(rootPath, callback) {
+  exports.parse(rootPath, function(err, cfg) {
+    if (err) {
+      return callback(err, false);
+    }
+    callback(undefined, cfg ? true : false);
+  });
+};
+
+exports.parse = function(rootPath, callback) {
+  var packageJson = path.resolve(rootPath, 'package.json');
+
+  fs.readFile(packageJson, {encoding: 'utf8', flag: 'r'}, function(err, data) {
+    if (err) {
+      return callback(err);
+    }
+
+    var cfg = parseJSON(data) || {};
+
+    callback(undefined, cfg.starterized);
+  });
+};
---
+++ test/config_test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var sinon = require('sinon');
+var expect = require('expect.js');
+var config = require('../config');
+var path = require('path');
+
+describe('config', function() {
+  var sandbox;
+
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  describe('exists', function() {
+    it('should be true when configuration found', function(done) {
+      var rootPath = path.resolve('test', 'fixtures', 'initialized-project');
+
+      config.exists(rootPath, function(err, exists) {
+        expect(exists).to.be.equal(true);
+        done();
+      });
+    });
+    it('should return err when package.json not found', function(done) {
+      var rootPath = path.resolve('test', 'fixtures', 'empty-project');
+
+      config.exists(rootPath, function(err) {
+        expect(err).to.be.ok();
+        done();
+      });
+    });
+  });
+
+  describe('parse', function() {
+    it('should return undefined when no package.json', function(done) {
+      var rootPath = path.resolve('test', 'fixtures', 'empty-project');
+
+      config.parse(rootPath, function(err, cfg) {
+        expect(cfg).to.be.equal(undefined);
+        done();
+      });
+    });
+  });
+});
