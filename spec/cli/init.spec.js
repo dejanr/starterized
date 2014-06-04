d2580612c8d20cf47965abf42b1e4c37c231dfce
Dejan Ranisavljevic <dejan.ranisavljevic@saturized.com>
2014-06-04 11:00:48 +0200

    Initialize each commander separately, and add force param to init cli.

--- spec/cli/init.spec.js
+++ spec/cli/init.spec.js
@@ -11,17 +11,22 @@ describe('$ starterized init <path>', function() {
 
   it('should initialize a project in cwd', function() {
     cli.argv(['node', '/usr/local/bin/starterized', 'init']);
-    expect(starterized.init).toHaveBeenCalledWith(process.cwd(), '');
+    expect(starterized.init).toHaveBeenCalledWith(process.cwd(), '', false);
+  });
+
+  it('should accept force flag', function() {
+    cli.argv(['node', '/usr/local/bin/starterized', 'init', '--force']);
+    expect(starterized.init).toHaveBeenCalledWith(process.cwd(), '', true);
   });
 
   it('should initialize a project in cwd at specified path', function() {
     cli.argv(['node', '/usr/local/bin/starterized', 'init', 'modul']);
-    expect(starterized.init).toHaveBeenCalledWith(process.cwd(), 'modul');
+    expect(starterized.init).toHaveBeenCalledWith(process.cwd(), 'modul', false);
   });
 
   it('should initialize a project at specified cwd and specified path', function() {
     cli.argv(['node', '/usr/local/bin/starterized', 'init', '--cwd', '~/Projects', 'demo']);
-    expect(starterized.init).toHaveBeenCalledWith('~/Projects', 'demo');
+    expect(starterized.init).toHaveBeenCalledWith('~/Projects', 'demo', false);
   });
 
 });
